a13d5fc3dce5478f76899c9aa0ff2e43ef4aa281
Gustavo Fernandez <sr1176626173@gmail.com>
2024-06-24 21:17:22 -0600

    seccion 16 creada

--- seccion15/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-cafe_seleccionado = seleccion_opciones_cafe()
-
-# material, cantidad = recursos_para_hacer_cafe(cafe_seleccionado)
-
-# print(f'Falta material: {material} , cantidad en maquina: {cantidad}')
