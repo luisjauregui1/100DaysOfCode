493cfd623d806d549f79116a28e94a1c6f232244
Gustavo Fernandez <sr1176626173@gmail.com>
2025-03-05 14:56:53 -0600

    Finalizacion de seccion 31

--- seccion31/main31.py
+++ seccion31/main31.py
@@ -4,57 +4,68 @@ import pandas
 import time
 
 BACKGROUND_COLOR = '#B1DDC6'
+current_card = {}
+to_learn = {}
 ruta = 'C:/Users/Gustavo/Documents/codigo/100DaysOfCode/seccion31/data/5000PalabrasIngles.csv'
 cart_front = "C:/Users/Gustavo/Documents/codigo/100DaysOfCode/seccion31/images/card_front.png"
 cart_back = "C:/Users/Gustavo/Documents/codigo/100DaysOfCode/seccion31/images/card_back.png"
-#--------------------------------Read CSV -----------------------------#
-data = pandas.read_csv(ruta)
-to_learn = data.to_dict(orient="records")
+wrong = "C:/Users/Gustavo/Documents/codigo/100DaysOfCode/seccion31/images/wrong.png"
+right = "C:/Users/Gustavo/Documents/codigo/100DaysOfCode/seccion31/images/right.png"
+safe = "C:/Users/Gustavo/Documents/codigo/100DaysOfCode/seccion31/data/word_to_learn.csv"
+# --------------------------------Read CSV -----------------------------#
+try:
+    data = pandas.read_csv(safe)
+except FileNotFoundError:
+    original_data = pandas.read_csv(ruta)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
 def next_cart():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = choice(to_learn)
+    canvas.itemconfig(card_title, text="English", fill='black')
+    canvas.itemconfig(card_word, text=current_card['English'], fill='black')
+    canvas.itemconfig(card_background, image=card_font_img)
+    flip_timer = window.after(3000, func=flip_card)
     
-    curretn_cart = choice(to_learn)
-    
-    canvas.itemconfig(card_title, text="English")
-    canvas.itemconfig(card_word, text=curretn_cart['English'])
 
-    
-    
+def flip_card():
+    canvas.itemconfig(card_title, text="Spanish", fill="white")
+    canvas.itemconfig(card_word, text=current_card["Spanish"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_image)
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv(safe, index=False)
+    next_cart()
     
 # -------------------------------UI SETUP -----------------------------#
 window = Tk()
 window.title("Flashy")
-window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
 
 # Canvas principal con grid
-canvas = Canvas(window, width=800, height=526)
-background = PhotoImage(file=cart_front)
-canvas.create_image(400, 263, image=background)
-canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=0, column=0, columnspan=2)
-
-
-# Texto de Canvas principal
+canvas = Canvas(window, width=800, height=526) 
+card_font_img = PhotoImage(file=cart_front)
+card_back_image = PhotoImage(file=cart_back)
+card_background = canvas.create_image(400, 263, image=card_font_img)
 card_title = canvas.create_text(400, 150, text="Idioma", fill="black", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 300, text="Word", fill='black', font=("Ariel", 60, 'bold'))
-
-photo_wrong = PhotoImage(file="C:/Users/Gustavo/Documents/codigo/100DaysOfCode/seccion31/images/wrong.png")
-unknown_bottom = Button(image=photo_wrong, command=next_cart)
-unknown_bottom.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+# boton x
+photo_wrong = PhotoImage(file=wrong)
+unknown_bottom = Button(image=photo_wrong, highlightthickness=0, command=next_cart)
 unknown_bottom.grid(row=1, column=0)
-
-photo_right = PhotoImage(file="C:/Users/Gustavo/Documents/codigo/100DaysOfCode/seccion31/images/right.png")
-known_bottom = Button(image=photo_right, command=next_cart)
-known_bottom.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-known_bottom.grid(row=1,column=1)
-
-# 3s
-new_image = PhotoImage(file=cart_back)
-old_image = PhotoImage(file=cart_front)
-canvas_image = canvas.create_image(300,300 ,image=old_image)
-
-# para cambiar la imagen:
-canvas.itemconfig(canvas_image, image=new_image)
+# boton ✔
+photo_right = PhotoImage(file=right)
+known_bottom = Button(image=photo_right, highlightthickness=0, command=is_known)
+known_bottom.grid(row=1, column=1)
 
 next_cart()
 
